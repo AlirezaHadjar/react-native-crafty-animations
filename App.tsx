e62fe67bed5315ce62b4023ba0258aeeb2dc4a01
Alireza Hadjar <asiahadjar@gmail.com>
2024-12-28 00:43:50 +0300

    chore: update dependencies, enable new architecture, and refine .gitignore

--- App.tsx
+++ App.tsx
@@ -8,8 +8,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import {ExampleScreens} from './src/screens';
 import {Home} from './src/Home';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
 
 const Stack = createNativeStackNavigator<StackParamList>();
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false, // Reanimated runs in strict mode by default
+});
 
 const App: React.FC = () => {
   return (
